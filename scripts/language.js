9578b33dee9b7f0b23ec4404ff4d066f726bfba2
Sergio C Castelani Jr <sergio.castelani@gmail.com>
2023-05-17 21:36:10 -0300

    Added english support

---
+++ scripts/language.js
@@ -0,0 +1,43 @@
+// Maintain a supported Language List
+var langTypes = ['en', 'pt'];
+// Get browser Language
+var userLang = localStorage.getItem("language") || 
+                navigator.language || 
+                navigator.userLanguage;
+// extract Language (en-US => en)
+userLang = userLang.substring(0, 2);
+// Call the function to set language
+changeLang(userLang);
+
+// function to change language
+function changeLang(lang) 
+{
+  localStorage.setItem("language", lang);
+
+  langTypes.forEach((langType) => 
+  {
+    // if language matches, display
+    if (langType == lang) 
+    {
+      var langElems = document.querySelectorAll('.' + langType);
+      langElems.forEach((elem) => 
+      {
+        elem.classList.remove("hide");
+      });
+    }
+    else 
+    {
+      hideLang(langType);
+    }
+  });
+}
+
+// function to hide language
+function hideLang(lang) 
+{
+  var langElems = document.querySelectorAll('.' + lang);
+  langElems.forEach((elem) => 
+  {
+    elem.classList.add("hide");
+  })
+}
